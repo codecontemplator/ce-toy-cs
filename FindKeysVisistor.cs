d04fe124b159062c63cadf0b1c5270e0468aab40
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2020-12-20 22:46:53 +0100

    Removed dead code

--- FindKeysVisistor.cs
+++ FindKeysVisistor.cs
@@ -8,20 +8,6 @@ namespace ce_toy_cs
     {
         public HashSet<string> FoundKeys = new HashSet<string>();
 
-        protected override Expression VisitBinary(BinaryExpression b)
-        {
-            if (b.NodeType == ExpressionType.AndAlso)
-            {
-                Expression left = this.Visit(b.Left);
-                Expression right = this.Visit(b.Right);
-
-                // Make this binary expression an OrElse operation instead of an AndAlso operation.  
-                return Expression.MakeBinary(ExpressionType.OrElse, left, right, b.IsLiftedToNull, b.Method);
-            }
-
-            return base.VisitBinary(b);
-        }
-
         protected override Expression VisitMethodCall(MethodCallExpression node)
         {
             if (node.Method.Name == "GetValue")
