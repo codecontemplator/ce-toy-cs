71d986b1b19d1ae14729fed5d582f6b748043ae5
mrange <marten_range@hotmail.com>
2021-01-01 21:18:18 +0100

    Added F# sample

---
+++ ce-toy-fs/CsCollectVariables/FsModel.fs
@@ -0,0 +1,69 @@
+﻿// -----------------------------------------------------------------------------
+// THIS FILE IS GENERATED FROM: FsModel.tt
+//  Changes to this file will be lost when the files is regenerated
+// -----------------------------------------------------------------------------
+
+namespace FsDepedencies
+
+
+module Model =
+  open System
+  open System.Collections.Generic
+
+  open Core
+
+  type [<Interface; Variable>] IAmountVariable = abstract Amount : decimal
+  let metaAmount = constVariable<IAmountVariable, decimal> "Amount" 0.0M
+  let getAmount (env : #IAmountVariable) = env.Amount
+  type [<Interface; Variable>] ICreditAVariable = abstract CreditA : decimal
+  let metaCreditA = constVariable<ICreditAVariable, decimal> "CreditA" 0.0M
+  let getCreditA (env : #ICreditAVariable) = env.CreditA
+  type [<Interface; Variable>] ICreditBVariable = abstract CreditB : decimal
+  let metaCreditB = constVariable<ICreditBVariable, decimal> "CreditB" 0.0M
+  let getCreditB (env : #ICreditBVariable) = env.CreditB
+  type [<Interface; Variable>] ICreditCVariable = abstract CreditC : decimal
+  let metaCreditC = constVariable<ICreditCVariable, decimal> "CreditC" 0.0M
+  let getCreditC (env : #ICreditCVariable) = env.CreditC
+
+  let metaVariables : MetaVariable array =
+    [|
+      metaAmount
+      metaCreditA
+      metaCreditB
+      metaCreditC
+    |]
+
+  let domainTypes =
+    metaVariables
+    |> Array.map (fun mv -> mv.DomainType, mv)
+    |> dict
+
+  type Env (evaluators : IDictionary<Type, MetaVariableEvaluator>) =
+    class
+      let lookup (mv : MetaVariable<'DT, 'VT>) : 'VT =
+        let name  = mv.Name
+        let dt    = mv.DomainType
+        let b, ue = evaluators.TryGetValue dt
+        if b then
+          match ue with
+          | (:? MetaVariableEvaluator<'DT, 'VT> as e) -> e.Eval ()
+          | v                                         ->
+            let lt = v.GetType()
+            let et = typeof<'VT>
+            failwithf "Variable '%s' loaded but type mismatch. Loaded: %s, requested: %s" name lt.Name et.Name
+        else
+          failwithf "Variable '%s' not loaded" name
+      interface IAmountVariable with
+        member x.Amount = lookup metaAmount
+      end
+      interface ICreditAVariable with
+        member x.CreditA = lookup metaCreditA
+      end
+      interface ICreditBVariable with
+        member x.CreditB = lookup metaCreditB
+      end
+      interface ICreditCVariable with
+        member x.CreditC = lookup metaCreditC
+      end
+    end
+  let env = Env (dict [||])
---
+++ ce-toy-fs/FsCollectVariables/Program.fs
@@ -0,0 +1,28 @@
+﻿open FsDepedencies
+open Model
+
+let absoluteMaxAmount env amountLimit =
+  let amount  = env |> getAmount
+
+  min amount amountLimit
+
+let maxTotalDebt env debtLimit =
+  let amount      = env |> getAmount
+  let creditA     = env |> getCreditA
+  let creditB     = env |> getCreditB
+  let totalCredit = creditA + creditB
+
+  if totalCredit > debtLimit then 0.0M else amount
+
+let printDependencies q = Core.printDependencies domainTypes q
+let invoke            q = Core.invoke domainTypes Env q
+
+[<EntryPoint>]
+let main argv =
+
+  printDependencies <@ absoluteMaxAmount env 100.0M @>
+  printDependencies <@ maxTotalDebt env 100.0M @>
+
+  printfn "Result: %A" <| invoke <@ maxTotalDebt env 100.0M @>
+
+  0
