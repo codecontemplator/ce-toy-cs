8c6c9a106f0d20df6ba19fa2cd8ca335c022005e
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2021-01-09 13:48:34 +0100

    Fix problem with not finding first variable in expression tree

--- ce-toy-cs/FindKeysVisistor.cs
+++ ce-toy-cs/FindKeysVisistor.cs
@@ -38,15 +38,29 @@ namespace ce_toy_cs
                     return node;
                 }
             }
+            if (node.Method.Name == "GetValueImpl")
+            {
+                var keysArg = node.Arguments[0] as MemberExpression;
+                var keysArgExpression = keysArg.Expression as ConstantExpression;
+                var sKeysFieldInfo = keysArgExpression.Value.GetType().GetField("key");
+                var key = sKeysFieldInfo.GetValue(keysArgExpression.Value) as string;
+                FoundKeys.Add(key);
+                return node;
+            }
             else if (node.Method.Name == "SEval")
             {
                 var keysArg = node.Arguments[2] as MemberExpression;
                 var keysArgExpression = keysArg.Expression as ConstantExpression;
                 var sKeysFieldInfo = keysArgExpression.Value.GetType().GetField("sKeys");
                 var keys = sKeysFieldInfo.GetValue(keysArgExpression.Value) as IEnumerable<string>;
-                foreach(var key in keys)
+                foreach (var key in keys)
                     FoundKeys.Add(key);
                 return node;
+            }
+            else if (node.Method.Name == "Select")
+            {
+                return base.VisitMethodCall(node);
+
             }
             else
                 return base.VisitMethodCall(node);
