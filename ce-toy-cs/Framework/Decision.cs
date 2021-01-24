4d763fd689234bc4dd63d40f19f35a14a3fa18fa
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2021-01-24 22:31:55 +0100

    Decision added

---
+++ ce-toy-cs/Framework/Decision.cs
@@ -0,0 +1,17 @@
+﻿namespace ce_toy_cs.Framework
+{
+    public enum DecisionType
+    {
+        Accept, Reject, AcceptGivenAmount
+    }
+
+    public record Decision
+    {
+        public DecisionType Type { get; init; }
+        public int? Amount { get; init; }
+
+        public static Decision Accept { get; } = new Decision { Type = DecisionType.Accept };
+        public static Decision Reject { get; } = new Decision { Type = DecisionType.Reject, Amount = 0 };
+        public static Decision AcceptGivenAmount(int amount) => new Decision { Type = DecisionType.AcceptGivenAmount, Amount = amount };
+    }
+}
